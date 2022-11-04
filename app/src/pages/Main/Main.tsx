62d42b6019d395a73a17f3271fd1664e1fe5770f
Dmitry Melnikov <dmitry.melnikov@changehealthcare.com>
2022-11-04 23:35:02 +0200

    chore(app): scrollable

--- app/src/pages/Main/Main.tsx
+++ app/src/pages/Main/Main.tsx
@@ -7,7 +7,7 @@ export function Main() {
   return (
     <div className={clsx('column', classes.container)}>
       <SprintHeader />
-      <div className='line width-fit'>
+      <div className={clsx('line', classes.sprints)}>
         <div className={classes['eng-header']}>left</div>
         <Sprints />
       </div>
